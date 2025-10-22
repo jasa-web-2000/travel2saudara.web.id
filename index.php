1366be55bd574f62a4c4b3445e4bc07ed4525e95
dion-zebua <zebuadbless@gmail.com>
2025-10-22 19:07:12 +0700

    malas

--- index.php
+++ index.php
@@ -152,6 +152,25 @@ $galeri = [
 
 <head>
 
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-TMLKN5ST');
+  </script>
+  <!-- End Google Tag Manager -->
+
   <meta name="author" content="Dion Zebua" />
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -186,6 +205,10 @@ $galeri = [
 </head>
 
 <body class="font-poppins text-slate-700 overflow-x-hidden">
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMLKN5ST"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <header class="">
     <div class="container !py-0">
       <div class="flex justify-between">
